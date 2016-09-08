0d36818ef042139f6a85d2e202dcbae5da9fe790
Tyler Smith <tyler.smith6891@gmail.com>
2016-09-08 15:50:29 -0700

    average ratings working

---
+++ ratings.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var ratings = [];
+var totalRating;
+var avgRating;
+
+var rateForm = document.getElementById('rating');
+rateForm.addEventListener('submit', submitRating);
+
+function submitRating() {
+  totalRating = 0;
+  event.preventDefault();
+  ratings.push(Number(event.target.rating.value));
+  for (var i = 0; i < ratings.length; i++) {
+    totalRating += ratings[i];
+    console.log('Total Rating:', totalRating);
+    avgRating = totalRating / ratings.length;
+    avgRating = (Math.round(avgRating * 2) / 2).toFixed(1);
+    console.log('Average Rating:', avgRating);
+  }
+}
